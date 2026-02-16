43da194550567ca70774d2c88e283e1bbfe4d9b6
nletteron <nicolas.letteron-ext@assurance-maladie.fr>
2026-02-16 15:27:30 +0100

    ErrorCatalog chargement après démarrage + ActeMetier chargé après les singletons pour gérer le cache du TraceSupervisor

--- ordonnance-infrastructure/src/main/java/fr/cnamts/cpam33/ordonnance/infrastructure/in/adapters/batch/init/ActeMetierStartup.java
+++ ordonnance-infrastructure/src/main/java/fr/cnamts/cpam33/ordonnance/infrastructure/in/adapters/batch/init/ActeMetierStartup.java
@@ -2,13 +2,15 @@ package fr.cnamts.cpam33.ordonnance.infrastructure.in.adapters.batch.init;
 
 import fr.cnamts.cpam33.ordonnance.infrastructure.in.adapters.batch.ActeMetierLoader;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ActeMetierStartup {
+public class ActeMetierStartup implements SmartInitializingSingleton {
 
     private final ActeMetierLoader acteMetierLoader;
 
@@ -16,8 +18,8 @@ public class ActeMetierStartup {
         this.acteMetierLoader = acteMetierLoader;
     }
 
-    @PostConstruct
-    public void init() {
+    @Override
+    public void afterSingletonsInstantiated() {
         acteMetierLoader.reload();
     }
 
--- ordonnance-infrastructure/src/main/java/fr/cnamts/cpam33/ordonnance/infrastructure/in/adapters/batch/init/ErrorCatalogStartup.java
+++ ordonnance-infrastructure/src/main/java/fr/cnamts/cpam33/ordonnance/infrastructure/in/adapters/batch/init/ErrorCatalogStartup.java
@@ -2,9 +2,14 @@ package fr.cnamts.cpam33.ordonnance.infrastructure.in.adapters.batch.init;
 
 import fr.cnamts.cpam33.ordonnance.infrastructure.in.adapters.batch.ErrorCatalogLoader;
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableAsync
 public class ErrorCatalogStartup {
 
     private final ErrorCatalogLoader errorCatalogLoader;
@@ -13,7 +18,8 @@ public class ErrorCatalogStartup {
         this.errorCatalogLoader = errorCatalogLoader;
     }
 
-    @PostConstruct
+    @Async
+    @EventListener(ApplicationReadyEvent.class)
     public void init() {
         errorCatalogLoader.reload();
     }
