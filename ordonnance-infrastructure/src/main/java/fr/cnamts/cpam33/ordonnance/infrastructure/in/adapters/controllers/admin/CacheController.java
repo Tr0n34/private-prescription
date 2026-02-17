815a7ccb7a533376d0c0dd323fa870682e47a4cc
nletteron <nicolas.letteron-ext@assurance-maladie.fr>
2026-02-17 16:03:36 +0100

    correction du cache des actes métiers + endpoint de vérification du cache

---
+++ ordonnance-infrastructure/src/main/java/fr/cnamts/cpam33/ordonnance/infrastructure/in/adapters/controllers/admin/CacheController.java
@@ -0,0 +1,35 @@
+package fr.cnamts.cpam33.ordonnance.infrastructure.in.adapters.controllers.admin;
+
+import fr.cnamts.cpam33.ordonnance.domain.models.tracabilite.ActeMetier;
+import fr.cnamts.cpam33.ordonnance.infrastructure.kernel.caches.ActeMetierCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/caches")
+public class CacheController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CacheController.class);
+
+    private final ActeMetierCache acteMetierCache;
+
+    public CacheController(ActeMetierCache acteMetierCache) {
+        this.acteMetierCache = acteMetierCache;
+    }
+
+    @GetMapping("/acteMetiers")
+    public ResponseEntity<?> getActeMetiers() {
+        return ResponseEntity.ok().body(acteMetierCache.snapshot());
+    }
+
+    @GetMapping("/acteMetiers/{code}")
+    public ResponseEntity<ActeMetier> getActeMetier(@PathVariable("code") String code) {
+        return ResponseEntity.ok().body(acteMetierCache.getRequired(code));
+    }
+
+}
